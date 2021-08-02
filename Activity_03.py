2b9b4fb8a05feb7324f99d10f6dc86e9dd58c2ea
Nandita2645 <71492935+Nandita2645@users.noreply.github.com>
2021-08-02 23:24:44 +0530

    write commit

---
+++ Activity_03.py
@@ -0,0 +1,8 @@
+>>> a=input("firststring:")
+firststring:Good
+>>> b=input("secondstring:")
+secondstring:Morning
+>>> c=a+b
+>>>print(c+"\n"+5*c)
+GoodMorning
+GoodMorningGoodMorningGoodMorningGoodMorningGoodMorning
