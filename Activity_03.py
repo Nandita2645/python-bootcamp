f4af10f74cae283c7ae644cf233f8232bd503457
Nandita2645 <71492935+Nandita2645@users.noreply.github.com>
2021-08-03 00:04:24 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,8 +1,6 @@
->>> a=input("firststring:")
-firststring:Good
->>> b=input("secondstring:")
-secondstring:Morning
->>> c=a+b
->>>print(c+"\n"+5*c)
+a=input("firststring:")
+b=input("secondstring:")
+c=a+b
+print(c+"\n"+5*c)
 GoodMorning
 GoodMorningGoodMorningGoodMorningGoodMorningGoodMorning
