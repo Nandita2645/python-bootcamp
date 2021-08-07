10d23c4b555eda6913c1694aee0898a96b47cac6
Nandita2645 <71492935+Nandita2645@users.noreply.github.com>
2021-08-07 19:33:33 +0530

    done commit

---
+++ Activity_12.py
@@ -0,0 +1,20 @@
+def inputnum():
+    num=int(input("enter number: "))
+    return num
+def cond(a,b,c):
+    if a>b and a>c:
+        print(f" %d is the greatest number among {a},{b} and {c}"%a)
+        return a
+    elif b>a and b>c:        
+        print(f" %d is the greatest number among {a},{b} and {c}"%b)
+        return b
+    else:        
+        print(f" %d is the greatest number among {a},{b} and {c}"%c)
+        return c
+def main():
+    a=inputnum()
+    b=inputnum()
+    c=inputnum()
+    cond(a,b,c)
+
+main()
